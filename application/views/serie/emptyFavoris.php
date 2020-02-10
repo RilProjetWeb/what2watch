b83254af9e2abd1c9db3b6f3233ea2bff73a4492
amelie saccomandi <amelie.saccomandi@viacesi.fr>
2020-02-10 16:08:04 +0100

    fix bug hover tooltip

--- application/views/serie/emptyFavoris.php
+++ application/views/serie/emptyFavoris.php
@@ -1,4 +1,4 @@
 <div class="container-general">
-	<h3>Vous n'avez pas encore ajouter de séries à votre liste... <a href="/what2watch/">Parcourir les séries maintenant</a></h3>	
+	<h3>Vous n'avez pas encore ajouter de séries à votre liste... <a class="link-red" href="/what2watch/">Parcourir les séries maintenant</a></h3>	
 </div>
 
