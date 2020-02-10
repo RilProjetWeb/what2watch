915c3ecc0f8c34f032744350e795ba7442fdcbe6
amelie saccomandi <amelie.saccomandi@viacesi.fr>
2020-02-10 15:31:17 +0100

    modif style pagination + lien

--- application/views/serie/emptyMySeries.php
+++ application/views/serie/emptyMySeries.php
@@ -1,4 +1,4 @@
 <div class="container-general">
-	<h3>Vous n'avez pas encore ajouter de séries... <a href="/what2watch/index.php/serie/addSerie">Ajouter maintenant votre série préférée!</a></h3>	
+	<h3>Vous n'avez pas encore ajouter de séries... <a class="link-red" href="/what2watch/index.php/serie/addSerie">Ajouter maintenant votre série préférée!</a></h3>	
 </div>
 
